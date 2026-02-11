a8870a6f79c67266a1f9347f55cbaca99d13d222
dancroker <160754622+dancroker@users.noreply.github.com>
2026-02-11 11:07:24 +0000

    Added Color Changing to Control Node

--- rhythm-game/Scripts/AreaNote.gd
+++ rhythm-game/Scripts/AreaNote.gd
@@ -1,12 +1,38 @@
 extends Area2D
+@onready var sprite_2d_2: Sprite2D = $Sprite2D2
 
-@export var speed : float = 200.0
+@export var speed : float
+@export var path : int
+@export var type : int
+var pos_start
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pos_start = position.y
+	if type == 1:
+		sprite_2d_2.self_modulate = Color.AQUA
+	elif type == 2:
+		sprite_2d_2.self_modulate = Color.CRIMSON
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y += speed * delta
+	if (path == 0):
+		position.y += speed * delta
+	if type == 1:
+		sprite_2d_2.self_modulate = Color.AQUA
+	elif type == 2:
+		sprite_2d_2.self_modulate = Color.CRIMSON
+
+func hit(type_hit : int):
+	if (type_hit == type):
+		print("Note Hit!")
+		if (path == 0):
+			position.y = pos_start
+			if (type == 1):
+				type = 2
+			else:
+				type = 1
+	else:
+		print("Wrong Hit!")
--- rhythm-game/Scripts/BasicControls.gd
+++ rhythm-game/Scripts/BasicControls.gd
@@ -1,15 +1,22 @@
 extends CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var note: Area2D = $"../../Note/Note"
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var max_delay : int
 
 var is_mouse_inside: bool = false
 enum{
 	None = 0,
-	Note = 1,
+	Single = 1,
 	Hold = 2,
 }
 var note_collide : int = None
 var collision_point
 var collision_info
+var blue_time_delay : int = 0
+var red_time_delay : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -28,28 +35,40 @@ func _process(delta: float) -> void:
 	if collision_point != 0:
 		print("Note")
 		
-#func _on_mouse_entered() -> void:
-	#is_mouse_inside = true
-#
-#func _on_mouse_exited() -> void:
-	#is_mouse_inside = false
+
+	sprite_2d.modulate = Color(1,1,1)
+	if (blue_time_delay >  0):
+		sprite_2d.modulate = Color.AQUA
+		print("colour")
+		blue_time_delay -= 1
+	if (red_time_delay > 0):
+		sprite_2d.modulate = Color.CRIMSON
+		red_time_delay -= 1
+
+
 	
 func _input(event: InputEvent) -> void:
 	if is_mouse_inside:
 		if event.is_action_pressed("Inner"):
 			print("1")
+			
+			if (blue_time_delay ==  0):
+				blue_time_delay = max_delay
+				
 			if note_collide != None:
-				print("Note Hit!")
+				note.hit(1)
 		elif event.is_action_pressed("Outer"):
 			print("2")
+			if (red_time_delay ==  0):
+				red_time_delay = max_delay
 			if note_collide != None:
-				print("Note Hit!")
+				note.hit(2)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "Note":
 		print("Note is inside hit zone!")
-		note_collide = Note
+		note_collide = Single
 
 
 func _on_area_2d_mouse_entered() -> void:

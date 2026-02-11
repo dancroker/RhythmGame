332ef0c461b21fc12d27dd8170d15aec4f1c91c8
dancroker <160754622+dancroker@users.noreply.github.com>
2026-02-11 10:00:45 +0000

    Note Tracking Implemented

--- rhythm-game/Scripts/BasicControls.gd
+++ rhythm-game/Scripts/BasicControls.gd
@@ -2,12 +2,19 @@ extends CharacterBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var is_mouse_inside: bool = false
+enum{
+	None = 0,
+	Note = 1,
+	Hold = 2,
+}
+var note_collide : int = None
 var collision_point
 var collision_info
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mouse_entered.connect(_on_mouse_entered)
-	mouse_exited.connect(_on_mouse_exited)
+	pass
+	#mouse_entered.connect(_on_mouse_entered)
+	#mouse_exited.connect(_on_mouse_exited)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,15 +28,39 @@ func _process(delta: float) -> void:
 	if collision_point != 0:
 		print("Note")
 		
-func _on_mouse_entered() -> void:
-	is_mouse_inside = true
-
-func _on_mouse_exited() -> void:
-	is_mouse_inside = false
+#func _on_mouse_entered() -> void:
+	#is_mouse_inside = true
+#
+#func _on_mouse_exited() -> void:
+	#is_mouse_inside = false
 	
 func _input(event: InputEvent) -> void:
 	if is_mouse_inside:
 		if event.is_action_pressed("Inner"):
 			print("1")
+			if note_collide != None:
+				print("Note Hit!")
 		elif event.is_action_pressed("Outer"):
 			print("2")
+			if note_collide != None:
+				print("Note Hit!")
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "Note":
+		print("Note is inside hit zone!")
+		note_collide = Note
+
+
+func _on_area_2d_mouse_entered() -> void:
+	is_mouse_inside = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	is_mouse_inside = false
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.name == "Note":
+		print("Note left hit zone!")
+		note_collide = None
